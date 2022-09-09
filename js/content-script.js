11b495af62735c9cdf2445d53f384a7c8e510e58
DESKTOP-EPTIJJL\azumi2020si <kodamano0621@gmail.com>
2022-09-09 17:08:06 +0900

    #押した回数をカウント

--- js/content-script.js
+++ js/content-script.js
@@ -1,14 +1,20 @@
+//chrome.storage.local.clear();
+let data = {};
 const ankerElements = document.querySelectorAll('a');
 //console.log(ankerElements);
+
+chrome.storage.local.get(null, (result) => {
+    data = result;
+    console.log(result);
+}); 
 
 ankerElements.forEach((ankerElement)=>{
    //console.log(ankerElement);
    //console.log(ankerElement.href);
-   // このurlはページ上のurlすべて
-   const urls = ankerElement.href;
    
    //ここでのeはclickされたもの1つのこと。
    let ankerFunc = (e) => {
+    // オブジェクトの分割代入 const currentTarget = e.currentTargetの略
     const { currentTarget } = e;
     //console.log(e);
     console.log(currentTarget);
@@ -20,7 +26,18 @@ ankerElements.forEach((ankerElement)=>{
     currentTarget.classList.add('overCss2');
     currentTarget.style.zIndex = '30';
 
-    };
+    const url =currentTarget.href;
+    if(data.hasOwnProperty(url)){
+     data[url].count += 1;
+    } else {
+        data[url] = {};
+        data[url].count = 1;
+    }
+    chrome.storage.local.set(data, function () {
+        console.log('Value is set to ' , data);
+    });
+
+   };
    ankerElement.addEventListener('click', ankerFunc);
 
 });
